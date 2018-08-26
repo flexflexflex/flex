04ceaedbb78165eeb19374469787660b32822cb9
kensay98 <rustambabadjanov@mail.ru>
2018-08-26 21:32:41 +0500

    Added static files in docker

--- flex/settings.py
+++ flex/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = './static'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
